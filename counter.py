0dd3da6e963b1f676e602c06e5ad618b4e5c51c6
Maiya Tracy <maiya.tracy@gmail.com>
2019-06-06 14:55:50 -0700

    updated

--- counter.py
+++ counter.py
@@ -5,26 +5,20 @@ app.secret_key = 'keep it secret, keep it safe' # set a secret key for security 
 # adding this method
 @app.route("/")
 def show_counter():
-    print(request.form)
-    if 'secret_key' in session:
-        print('key exists!')
-        session['session_num']+=1
-    else:
-        print("key 'secret_key' does NOT exist")
-        session['session_num'] = 1
-    return render_template("index.html", session_num = session['session_num'])
+    if not "session_num" in session:
+        print("key 'session_num' does NOT exist")
+        session['session_num'] = 0
+    session['session_num']+=1
+    inputTwo = request.button['addTwo']
+    print(inputTwo)
 
-@app.route('/process', methods=['POST'])
-def nsession():
-    print("Got Post Info")
-    print(request.form)
 
-    return redirect("/stuff")	# changed this line!
+    return render_template("index.html", session_num = session["session_num"])
 
-@app.route('/stuff')
+@app.route('/destroysession')
 def destroy_session():
     session.clear()		# clears all keys
-    session.pop('secret_key')		# clears a specific key
+
     return redirect("/")	# changed this line!
 
 if __name__ == "__main__":
